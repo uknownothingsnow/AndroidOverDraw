d665c31c8a639914e410163f979e3a44b10b4ba8
zhenyue.lzy <zhenyue.lzy@alibaba-inc.com>
2016-01-25 01:07:42 +0800

    revert refactor

--- app/src/main/java/com/github/lzyzsd/androidoverdraw/MainActivity.java
+++ app/src/main/java/com/github/lzyzsd/androidoverdraw/MainActivity.java
@@ -26,7 +26,6 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getWindow().setBackgroundDrawable(null);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
